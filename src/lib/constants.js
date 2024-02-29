e9e006f302f18958fd319853918b3954768341fc
Delba de Oliveira <delbabrown@gmail.com>
2024-02-29 12:35:34 +0000

    Update constants.js

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,1 @@
-export const API_URL =
-  process.env.NODE_ENV === "production"
-    ? `https://${process.env.REACT_APP_VERCEL_URL}/api`
-    : "http://localhost:5001/api"
+export const API_URL = "http://localhost:5001/api"
